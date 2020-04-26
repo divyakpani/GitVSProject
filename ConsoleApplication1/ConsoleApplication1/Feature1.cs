721442f4c4c8b26b1971a7e587eecd5c911f0bed
divyakpani <divyakpani08@gmail.com>
2020-04-26 12:37:00 +0530

    Update Feature1.cs

--- ConsoleApplication1/ConsoleApplication1/Feature1.cs
+++ ConsoleApplication1/ConsoleApplication1/Feature1.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication1
         {
             var x1 = 1;
             var x2 = 2;
-            var sum = x1 + x2 ;
+            var sum = x1 + x2;
             return sum;
         }
     }
